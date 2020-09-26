fc307ff3416cd537a7be766dc522a9c30f316ddd
Simon Skorna <sskorna001@cee.ema.ad.pwcinternal.com>
2020-09-26 11:22:00 +0200

    added mu to GARCH estimator

--- util/GarchEstimator.py
+++ util/GarchEstimator.py
@@ -36,7 +36,7 @@ class GarchEstimator:
         sigma_sqr[0:3] = 0.1
         # loop through all values of sigma
         for i in range(3, T):
-            sigma_sqr[i] = theta[0] + theta[1] * time_series[i-1] ** 2 + theta[2] * time_series[i-2] ** 2 + theta[3] * sigma_sqr[i-1] + theta[4] * sigma_sqr[i-2] + theta[5] * sigma_sqr[i-3]
+            sigma_sqr[i] = theta[1] + theta[2] * time_series[i-1] ** 2 + theta[3] * time_series[i-2] ** 2 + theta[4] * sigma_sqr[i-1] + theta[5] * sigma_sqr[i-2] + theta[6] * sigma_sqr[i-3]
         
         return sigma_sqr
         
@@ -46,7 +46,7 @@ class GarchEstimator:
             sigma_sqr = self.garch_filter(time_series=time_series, theta=theta)
             sigma = np.sqrt(sigma_sqr)
         
-        llik = -(1/2) * np.log(2 * np.pi) - (1/2) * np.log(sigma**2) - (1/2) * ((time_series ** 2) / sigma ** 2)
+        llik = -(1/2) * np.log(2 * np.pi) - (1/2) * np.log(sigma**2) - (1/2) * (((time_series - theta[0]) ** 2) / sigma ** 2)
         
         if optim == True :
             return -sum(llik)
@@ -54,7 +54,7 @@ class GarchEstimator:
             return sum(llik)
         
     def fit_model(self, time_series, method):
-        bounds = Bounds([0.000001, 0, 0, 0, 0, 0],[100, 100, 100, 100, 100, 100])
+        bounds = Bounds([-10, 0.000001, 0, 0, 0, 0, 0],[10, 100, 100, 100, 100, 100, 100])
         x0 = list(self.theta_init.values())
         optim = True
 
@@ -63,16 +63,17 @@ class GarchEstimator:
             x0 = x0, 
             args = (time_series, method, optim),
             method = 'trust-constr',
-            options = {'gtol' : 1e-8, 'xtol' : 1e-8, 'maxiter' : 1000, 'disp' : True},
+            options = {'gtol' : 1e-2, 'xtol' : 1e-2, 'maxiter' : 100, 'disp' : True},
             bounds = bounds
         )
         self.theta = {
-            'alpha' : res.x[0],
-            'beta1' : res.x[1],
-            'beta2' : res.x[2],
-            'omega1' : res.x[3],
-            'omega2' : res.x[4],
-            'omega3' : res.x[5],
+            'mu' : res.x[0],
+            'alpha' : res.x[1],
+            'beta1' : res.x[2],
+            'beta2' : res.x[3],
+            'omega1' : res.x[4],
+            'omega2' : res.x[5],
+            'omega3' : res.x[6],
         }
         print('Model recalculated!')
         print(res)
@@ -101,8 +102,12 @@ class GarchEstimator:
             self.llik = self.calc_llik(theta=list(self.theta.values()), time_series=data_fit, method=method)
             
         return sigma_sqr
+    def get_AIC(self): 
         
+        try: 
+            self.llik
         
+        except AttributeError as error:
+            raise AttributeError('Log-likelihood missing (model needs to be fitted)')
         
-        
-        
+        return -2 * self.llik + 2*self.theta.length
